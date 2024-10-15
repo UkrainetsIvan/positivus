239fbc44999e96d5fcc7185d0a89c4ff5e84b51f
Українець Іван <127292648+UkrainetsIvan@users.noreply.github.com>
2024-10-15 02:44:46 -0700

    update slide

--- src/app/components/testimonails/testimonails.component.ts
+++ src/app/components/testimonails/testimonails.component.ts
@@ -15,37 +15,37 @@ export class TestimonailsComponent {
   slides = [
     {
       image: '/img/Bubble.svg',
-      description: '"fgsdhfghWe have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+      description: '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
       name: 'John Smith',
       proffession: 'Marketing Director at XYZ Corp'
     },
     {
       image: '/img/Bubble.svg',
-      description: '"gnghkjhkhjWe have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+      description: '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
       name: 'John Smith',
       proffession: 'Marketing Director at XYZ Corp'
     },
     {
       image: '/img/Bubble.svg',
-      description: '"agfdsgfdgsdfgWe have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+      description: '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
       name: 'John Smith',
       proffession: 'Marketing Director at XYZ Corp'
     },
     {
       image: '/img/Bubble.svg',
-      description: '"dfghhgshgdWe have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+      description: '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
       name: 'John Smith',
       proffession: 'Marketing Director at XYZ Corp'
     },
     {
       image: '/img/Bubble.svg',
-      description: '"hsghgsfhgfWe have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+      description: '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
       name: 'John Smith',
       proffession: 'Marketing Director at XYZ Corp'
     }
   ];
 
-  currentSlideIndex: number = 5;
+  currentSlideIndex: number = 1;
   arrowLeft: string = '/img/arrowLeft.svg';
   arrowRight: string = '/img/arrowRight.svg';
   greenStar: string = '/img/greenStar.svg';
@@ -53,8 +53,8 @@ export class TestimonailsComponent {
   stars: number[] = new Array(5);
   private two: any;
 
-  get visibleSlides() {
-    return this.slides.slice(this.currentSlideIndex, this.currentSlideIndex + 1);
+  get sliderTransform(): string {
+    return `translateX(-${this.currentSlideIndex * 50}%)`;
   }
 
   isActive(index: number): boolean {
